af93217f6836bc16400b5d054c4d6b23f261ce20
Collin Cunningham <me@collinmel.com>
2014-07-30 21:14:04 -0400

    refactoring,  comments & cleanup

--- OONTZ_Arpy.ino
+++ OONTZ_Arpy.ino
@@ -1,113 +1,106 @@
-// Super-basic OONTZ MIDI example.  Maps OONTZ buttons to MIDI note
-// on/off events; this is NOT a sequencer or anything fancy.
-// Requires an Arduino Leonardo w/TeeOnArdu config (or a PJRC Teensy),
-// software on host computer for synth or to route to other devices.
+// OONTZ ARPY
+// Arpeggiator for the Adafruit OONTZ & HELLA OONTZ
+// Plays a MIDI note sequence relative to button pressed
+//
+// For HELLA OONTZ change HELLA value to 1 (line 20)
+// For external MIDI clock sync change EXT_CLOCK value to 1 (line 21)
+//
+// Optional:
+//      Pattern control - connect rotary encoder to pins 4 & 5
+//      Tempo control   - connect rotary encoder to pins 8 & 9
+//
+
+void setup();   // Added to avoid Arduino IDE #ifdef bug
 
 #include <Wire.h>
 #include <Adafruit_Trellis.h>
 #include <Adafruit_OONTZ.h>
 
-#define LED     13 // Pin for heartbeat LED (shows code is working)
-#define CHANNEL 1  // MIDI channel number
+#define LED       13 // Pin for heartbeat LED (shows code is working)
+#define CHANNEL   1  // MIDI channel number
+#define HELLA     0  // 0 for standard OONTZ, 1 for HELLA OONTZ
+#define EXT_CLOCK 0  // 0 for internal clock, 1 for external
 
-//#define HELLA   1  // If this be HELLA OONTZ
-
-// A standard OONTZ has four Trellises in a 2x2 arrangement
-// (8x8 buttons total).  addr[] is the I2C address of the upper left,
-// upper right, lower left and lower right matrices, respectively,
-// assuming an upright orientation, i.e. labels on board are in the
-// normal reading direction.
-// A HELLA OONTZ has eight Trellis boards...
-
-//Standard OONTZ
-//Adafruit_Trellis T[4];
-//OONTZ            oontz(&T[0], &T[1], &T[2], &T[3]);
-//const uint8_t    addr[] = { 0x70, 0x71,
-//                            0x72, 0x73 };
-
-// HELLA OONTZ
+#if HELLA
 Adafruit_Trellis T[8];
 OONTZ            oontz(&T[0], &T[1], &T[2], &T[3],
                        &T[4], &T[5], &T[6], &T[7]);
 const uint8_t    addr[] = { 0x70, 0x71, 0x72, 0x73,
                             0x74, 0x75, 0x76, 0x77 };
+#else
+Adafruit_Trellis T[4];
+OONTZ            oontz(&T[0], &T[1], &T[2], &T[3]);
+const uint8_t    addr[] = { 0x70, 0x71,
+                            0x72, 0x73 };
+#endif
 
-// For this example, MIDI note numbers are simply centered based on
-// the number of Trellis buttons; each row doesn't necessarily
-// correspond to an octave or anything.
-#define WIDTH     ((sizeof(T) / sizeof(T[0])) * 2)
-#define N_BUTTONS ((sizeof(T) / sizeof(T[0])) * 16)
-#define LOWNOTE   ((128 - N_BUTTONS) / 2)
+#define WIDTH      ((sizeof(T) / sizeof(T[0])) * 2)
+#define HEIGHT     (N_BUTTONS / WIDTH)
+#define N_BUTTONS  ((sizeof(T) / sizeof(T[0])) * 16)
+#define ARP_NOTES  6
+#define NULL_INDEX 255
 
 uint8_t       heart        = 0;  // Heartbeat LED counter
 unsigned long prevReadTime = 0L; // Keypad polling timer
+//bool          extClock = true;
+uint8_t       quantDiv = 8;      // Quantization division, 2 = half note
+uint8_t       clockPulse = 0;
 
-//Arpy stuff
-#define HEIGHT  (N_BUTTONS / WIDTH)
-#define ARP_NOTES 6
-#define INVALID_BUTTON_INDEX 255
-bool extClock = true;
-uint8_t quantDiv = 8;  // Quantization division, 2 = half note
-#define QUANT_PULSE (96/quantDiv)  // Number of pulses per quantization division
+#define QUANT_PULSE (96/quantDiv)// Number of pulses per quantization division
 
-uint8_t noteDivs[6] = {
-    1,2,4,8,16,32
-};
+uint8_t       noteDivs[] = { 1,2,4,8,16,32};
 
-uint8_t clockPulse = 0;
-bool    pulseHi    = false;
-
-//#define NOTE_LENGTH 240L //note's length in milliseconds
-
-int8_t arpA[ARP_NOTES][2] = {
+// Note patterns for arpeggiator
+int8_t
+arpA[ARP_NOTES][2] = {
     {  0,   0  },
     {  -1,  -1 },
     {  1,   -1 },
     {  1,   1  },
     {  -1,  1  },
     {  -1,  1  },
-};
-int8_t arpB[ARP_NOTES][2] = {
+},
+arpB[ARP_NOTES][2] = {
     {  0,   0  },
     {  1,   -1 },
     {  2,   -2 },
     {  2,   -2 },
     {  1,   -1 },
     {  2,   -1 },
-};
-int8_t arpC[ARP_NOTES][2] = {
+},
+arpC[ARP_NOTES][2] = {
     {  0,   0  },
     {  -1,  0  },
     {  0,   0  },
     {  0,   -1 },
     {  0,   0  },
     {  1,   0  },
-};
-int8_t arpD[ARP_NOTES][2] = {
+},
+arpD[ARP_NOTES][2] = {
     {  0,   0 },
     {  1,   0 },
     {  0,   0 },
     {  1,   0 },
     {  1,   2 },
     {  1,   2 }
-};
-int8_t arpE[ARP_NOTES][2] = {
+},
+arpE[ARP_NOTES][2] = {
     {  0,   0 },
     {  1,   0 },
     {  2,   0 },
     {  3,   0 },
     {  3,   1 },
     {  2,   1 }
-};
-int8_t arpF[ARP_NOTES][2] = {
+},
+arpF[ARP_NOTES][2] = {
     {  0,   0 },
     {  1,   0 },
     {  -1,  0 },
     {  2,   0 },
     {  -2,  0 },
     {  0,   0 }
-};
-int8_t arpG[ARP_NOTES][2] = {
+},
+arpG[ARP_NOTES][2] = {
     {  0,   0 },
     {  1,   -1 },
     {  2,  0 },
@@ -116,49 +109,32 @@ int8_t arpG[ARP_NOTES][2] = {
     {  1,   0 }
 };
 
-int8_t (*arpCollection[])[ARP_NOTES][2] = {
-    &arpA,
-    &arpB,
-    &arpC,
-    &arpD,
-    &arpE,
-    &arpF,
-    &arpG
-};
+int8_t (*arpCollection[])[ARP_NOTES][2] = { &arpA, &arpB, &arpC, &arpD,
+                                            &arpE, &arpF, &arpG };
 
 uint8_t arpCount = sizeof(arpCollection) / sizeof(arpCollection[0]);
-
 int8_t (*arp)[ARP_NOTES][2] = &(*arpCollection)[0];
-
-uint8_t pitchMapA[N_BUTTONS] = {    //Test note map for 8x8
-    36, 38, 39, 41, 43, 46, 48, 50,
-    41, 43, 44, 46, 48, 51, 53, 55,
-    46, 48, 50, 51, 53, 55, 58, 60,
-    51, 53, 55, 56, 58, 60, 63, 65,
-    55, 58, 60, 62, 63, 65, 67, 70,
-    60, 63, 65, 67, 68, 70, 72, 75,
-    65, 67, 70, 72, 74, 75, 77, 79,
-    70, 72, 75, 77, 79, 80, 82, 84
-};
-
 uint8_t pitchMap[N_BUTTONS];
 
-// Musical modes / scales
-uint8_t ionian[12]      = { 0, 0, 2, 4, 4, 5, 5, 7, 7, 9,11,11},
-        dorian[12]      = { 0, 0, 2, 3, 3, 5, 5, 7, 7, 9,10,10},
-        phrygian[12]    = { 0, 1, 2, 3, 3, 5, 5, 7, 8, 8,10,10},
-        lydian[12]      = { 0, 0, 2, 2, 4, 4, 6, 7, 7, 9,10,11},
-        mixolydian[12]  = { 0, 0, 2, 2, 4, 5, 5, 7, 7, 9,10,10},
-        aeolian[12]     = { 0, 0, 2, 3, 3, 5, 5, 7, 8, 8,10,10},
-        locrian[12]     = { 0, 1, 1, 3, 3, 5, 6, 6, 8, 8,10,10};
+// Musical mode/scale intervals
+uint8_t chromatic[12]   = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11 },
+        ionian[12]      = { 0, 0, 2, 4, 4, 5, 5, 7, 7, 9,11,11 },
+        dorian[12]      = { 0, 0, 2, 3, 3, 5, 5, 7, 7, 9,10,10 },
+        phrygian[12]    = { 0, 1, 2, 3, 3, 5, 5, 7, 8, 8,10,10 },
+        lydian[12]      = { 0, 0, 2, 2, 4, 4, 6, 7, 7, 9,10,11 },
+        mixolydian[12]  = { 0, 0, 2, 2, 4, 5, 5, 7, 7, 9,10,10 },
+        aeolian[12]     = { 0, 0, 2, 3, 3, 5, 5, 7, 8, 8,10,10 },
+        locrian[12]     = { 0, 1, 1, 3, 3, 5, 6, 6, 8, 8,10,10 };
+
+uint8_t (*scale)[12]    = &locrian;    // Chosen scale
 
 uint8_t firstNote = 36,  // First note, upper left of grid
         colIntvl  = 2,   // Semitones between each column
         rowIntvl  = 5;   // Semitones between each row
 
-boolean pressedButtonIndex[N_BUTTONS] = {false};   //button being virtually pressed for each sustained button press
-uint8_t arpSeqIndex[N_BUTTONS] = {INVALID_BUTTON_INDEX},
-        arpButtonIndex[N_BUTTONS] = {INVALID_BUTTON_INDEX};   //button being virtually pressed for each sustained button press
+boolean pressedButtonIndex[N_BUTTONS] = {false};        // Pressed state for each button
+uint8_t arpSeqIndex[N_BUTTONS]        = {NULL_INDEX},   // Current place in button arpeggio sequence
+        arpButtonIndex[N_BUTTONS]     = {NULL_INDEX};   // Button index being played for each actual pressed button
 
 // Encoders for tempo and arpeggio selection
 enc tempoEncoder(8, 9);
@@ -169,7 +145,7 @@ unsigned long beatInterval = 60000L / bpm, // ms/beat
 uint8_t       arpVelocityMax  = 102,
               arpVelocityMin  = 65;
 
-void setup() {
+void setup(){
     
     pinMode(LED, OUTPUT);
     
@@ -194,16 +170,17 @@ void setup() {
     oontz.clear();
     oontz.writeDisplay();
     
-    if (extClock == false) {
-        // Set up tempo encoder for Internal clock mode
-        tempoEncoder.setBounds(60 * 4, 480 * 4 + 3); // Set tempo limits
-        tempoEncoder.setValue(bpm * 4);              // *4's for encoder detents
-    }
-    else{
-        // Set up tempo encoder for External clock mode
-        tempoEncoder.setBounds(0, sizeof(noteDivs) / sizeof(noteDivs[0]) * 4 - 1, true); // Set tempo limits
-        tempoEncoder.setValue(3 * 4);        // *4's for encoder detents
-    }
+#if EXT_CLOCK
+    // Set up tempo encoder for External clock mode
+    uint8_t divCount = sizeof(noteDivs) / sizeof(noteDivs[0]) -1;
+    tempoEncoder.setBounds(0, divCount * 4, true); // Set tempo limits
+    tempoEncoder.setValue(3 * 4);                  // *4's for encoder detents
+#else
+    // Set up tempo encoder for Internal clock mode
+    tempoEncoder.setBounds(60 * 4, 480 * 4 + 3); // Set tempo limits
+    tempoEncoder.setValue(bpm * 4);              // *4's for encoder detents
+#endif
+
     
     //Set up arpeggio pattern encoder
     arpEncoder.setBounds(0, arpCount * 4 - 1, true);
@@ -215,7 +192,7 @@ void setup() {
 }
 
 
-void loop() {
+void loop(){
     
     checkMidi();
     
@@ -248,8 +225,14 @@ void loop() {
         }
         
         //iterate array, play arp sequence for pressed buttons
+        
+#if EXT_CLOCK
+        // EXTERNAL CLOCK
+        //Set note length from encoder value
+        quantDiv     = noteDivs[tempoEncoder.getValue() / 4];
+        
+#else
         // INTERNAL CLOCK
-        if (extClock == false) {
             if ((t - prevArpTime) >= beatInterval) {
                 
                 for (uint8_t i=0; i < N_BUTTONS; i++) {
@@ -263,13 +246,9 @@ void loop() {
             //Set Tempo from encoder value
             bpm          = tempoEncoder.getValue() / 4; // Div for encoder detents
             beatInterval = 60000L / bpm;
-        }
+#endif
         
-        // EXTERNAL CLOCK
-        else{
-            //Set note length from encoder value
-            quantDiv     = noteDivs[tempoEncoder.getValue() / 4];
-        }
+        
         
         //Set current arp notes
         int16_t arpIndex = arpEncoder.getValue() / 4;
@@ -287,29 +266,28 @@ void loop() {
 
 
 void checkMidi(){
-    
-    // INTERNAL CLOCK
-    if (extClock == false) {
-        while(usbMIDI.read()); // Discard incoming MIDI messages
-    }
-    
+
+#if EXT_CLOCK
     // EXTERNAL CLOCK
-    else{
-        if (usbMIDI.read() && usbMIDI.getType() == 8){   // Respond to clock signal
-            pulseHi = true;
-            clockPulse++;
-            if (clockPulse > 96) {   //MIDI clock sends 64 pulses per note
-                clockPulse = 1;
-            }
-            else if ((clockPulse % QUANT_PULSE) == 0){
-                for (uint8_t i=0; i < N_BUTTONS; i++) {
-                    if (pressedButtonIndex[i]) {
-                        playArp(i);
-                    }
+    if (usbMIDI.read() && usbMIDI.getType() == 8){   // Respond to clock signal
+        clockPulse++;
+        if (clockPulse > 96) {   //MIDI clock sends 64 pulses per note
+            clockPulse = 1;
+        }
+        else if ((clockPulse % QUANT_PULSE) == 0){
+            for (uint8_t i=0; i < N_BUTTONS; i++) {
+                if (pressedButtonIndex[i]) {
+                    playArp(i);
                 }
             }
         }
     }
+    
+#else
+    // INTERNAL CLOCK
+    while(usbMIDI.read()); // Discard incoming MIDI messages
+
+#endif
     
 }
 
@@ -330,7 +308,7 @@ void writePitchMap(){
     for (int i = 0; i < N_BUTTONS; i++) {
         uint8_t deg = pitchMap[i] % 12;
         uint8_t oct = pitchMap[i] / 12;
-        pitchMap[i] = oct * 12 + aeolian[deg];
+        pitchMap[i] = oct * 12 + (*scale)[deg];
     }
     
     //Test notes
@@ -400,7 +378,7 @@ void stopArp(uint8_t button){
     stopNoteForButton(arpButtonIndex[button]);
     
     //store an invalid button index in its place
-    arpSeqIndex[button] = INVALID_BUTTON_INDEX;  //check for invalid
+    arpSeqIndex[button] = NULL_INDEX;  //check for invalid
     
 }
 
@@ -418,8 +396,6 @@ uint8_t findNoteFromIndex(uint8_t buttonIndex){
 uint8_t findNoteFromXY(uint8_t x, uint8_t y){
     
     return pitchMap[y * WIDTH + x];
-    
-//    return LOWNOTE + y * WIDTH + x;
     
 }
 
@@ -454,22 +430,3 @@ void flashDebug(uint8_t count){
     
 }
 
-
-/* Here's the set of MIDI functions for making your own projects:
- 
- usbMIDI.sendNoteOn(note, velocity, channel)
- usbMIDI.sendNoteOff(note, velocity, channel)
- usbMIDI.sendPolyPressure(note, pressure, channel)
- usbMIDI.sendControlChange(control, value, channel)
- usbMIDI.sendProgramChange(program, channel)
- usbMIDI.sendAfterTouch(pressure, channel)
- usbMIDI.sendPitchBend(value, channel)
- usbMIDI.sendSysEx(length, array)
- usbMIDI.send_now()
- 
- Some info on MIDI note numbers can be found here:
- http://www.phys.unsw.edu.au/jw/notes.html
- 
- Rather than MIDI, one could theoretically try using Serial to
- create a sketch compatible with serialosc or monomeserial, but
- those tools have proven notoriously unreliable thus far. */
